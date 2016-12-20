5bb18a042ee973501e334a781aed8cfd24affb8a
IlyaOvcharenko <ibovcharenko@gmail.com>
2016-12-20 14:10:46 +0300

    Client

---
+++ Source/RoomsBookingClient/RoomsBookingClient/App_Start/BundleConfig.cs
@@ -0,0 +1,43 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace RoomsBookingClient
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/kendo/js")
+                .Include("~/Scripts/kendo/2014.1.318/kendo.web.min.js")
+                .Include("~/Scripts/kendo/2014.1.318/cultures/kendo.culture.en-US.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+                "~/Scripts/knockout-{version}.js",
+                "~/Scripts/knockout.validation.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/kendo/css")
+               .Include("~/Content/kendo/2014.1.318/kendo.common.min.css", new CssRewriteUrlTransform())
+               .Include("~/Content/kendo/2014.1.318/kendo.silver.min.css", new CssRewriteUrlTransform()));
+        }
+    }
+}
